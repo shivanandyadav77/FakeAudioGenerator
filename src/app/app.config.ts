ff27e9ad46f680043b806b2e5a5f889ab0fc63fa
shivanandyadav77 <shivanandyadav77@gmail.com>
2024-09-11 17:04:40 +0530

    Changes done for ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,8 @@ export interface ApplicationConfig {
 }
 
 export const CONFIG: ApplicationConfig = {
-  apiURL: 'http://127.0.0.1:5000/uploadfile'
+  // apiURL: 'http://127.0.0.1:5000/uploadfile'
   // apiURL: 'https://falshapp-aydxhkeagvamgkak.southindia-01.azurewebsites.net/uploadfile'
   
-    // apiURL: 'https://demoaudiogenerationservice-fdbhbdh7ghbgd4da.southindia-01.azurewebsites.net/uploadfile'
+    apiURL: 'https://demoaudiogenerationservice-fdbhbdh7ghbgd4da.southindia-01.azurewebsites.net/uploadfile'
 };
